734e5f74feef72f8fba3b7b4f52fcd81e4ba68b2
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-03 16:08:26 -0300

    Refactor routes: update baseResponser to sayHelloWorld and add 404 NotFound error handling

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,30 @@
 import { Router } from 'express';
-import { ResponseDto } from '../dto/response/ResponseDto'
+import { ResponseDto } from '../dto/response/ResponseDto';
+import { listRouter } from './list/ListRouter';
+import { NotFound } from '../errors/http/NotFound';
 
-const routes = new Router();
 
 /**
- * Simple responser
+ * Simple responser placeholder
  * @param {Request} req
  * @param {Response} res
- */
-async function baseResponser(req, res) {
+*/
+async function sayHelloWorld(req, res) {
   const response = new ResponseDto(200, 'Hello World',  {
-    name : 'Hello World',
     message : 'Hello World',
-    something : 'Hello World'
   });
-
+  
   res.status(200).json(response);
 }
 
-routes.get('/', baseResponser);
+// start of routes
+const routes = new Router();
+routes.get('/', sayHelloWorld);
+routes.use('/list', listRouter);
+
+// Catch-all route for handling 404 errors
+routes.use((req, res, next) => {
+  throw new NotFound('Route doesn\'t exist');
+});
 
 export default routes;
