c48abcc77454a219178b17b24310b934b4bd03a5
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-03 20:32:30 -0300

    Add Jest plugin to ESLint configuration and update package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,32 @@
 module.exports = {
-    "env": {
-        "es6": true,
-        "node": true
+    env: {
+        es6: true,
+        node: true,
+        jest: true,
     },
-    "extends": [
-        "airbnb-base"
+    extends: [
+        'airbnb-base',
+        'plugin:jest/recommended',
     ],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
+    globals: {
+        Atomics: 'readonly',
+        SharedArrayBuffer: 'readonly',
+        module: 'readonly',
+        process: 'readonly',
     },
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
+    parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: 'module',
     },
-    "rules": {
-      "class-methods-use-this": "off",
-      "no-underscore-dangle": "off",
-      "no-unused-vars": [
-        "error",{ "ignoreRestSiblings": true, "argsIgnorePattern": "^_" }
-      ],
-      'import/prefer-default-export':'off',
-    }
+    rules: {
+        'class-methods-use-this': 'off',
+        'no-underscore-dangle': 'off',
+        'no-unused-vars': [
+            'error', { ignoreRestSiblings: true, argsIgnorePattern: '^_' },
+        ],
+        'import/prefer-default-export': 'off',
+    },
+    plugins: [
+        'jest',
+    ],
 };
