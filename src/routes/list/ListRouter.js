a50c9da2da3ba6553b9981b598c5f3b405a5410d
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-04 17:33:07 -0300

    Add authentication middleware to createList route in ListRouter

--- src/routes/list/ListRouter.js
+++ src/routes/list/ListRouter.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import { ListController } from '../../controllers/ListController';
 import { ListService } from '../../services/ListService';
+import { authMiddleware } from '../../middlewares/AuthMiddleware';
 
 const router = Router();
 const listService = new ListService();
 const listController = new ListController(listService);
 
-router.post('/', (req, res, next) => listController.createList(req, res, next));
+router.post('/', authMiddleware, listController.createList);
 router.get('/', (req, res, next) => listController.getLists(req, res, next));
 router.get('/:id', (req, res, next) => listController.getListById(req, res, next));
 router.put('/:id', (req, res, next) => listController.updateList(req, res, next));
