0abd8795e454003f50cbb92e3ededa9ceb1a3107
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-04 17:31:44 -0300

    Add user association and password hashing to List and User schemas

--- src/database/models/ListSchema.js
+++ src/database/models/ListSchema.js
@@ -1,8 +1,23 @@
 import mongoose from 'mongoose';
+import { User } from './UserSchema';
 
 export const listSchema = new mongoose.Schema({
     name: { type: String, required: true, unique: true, trim: true },
-    items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ToDo' }]
+    items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'ToDo' }],
+    user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+});
+
+// Middleware to link the list to the user before saving
+listSchema.pre('save', async function (next) {
+    if (this.isNew) {
+        const user = await User.findById(this.user);
+        if (!user) {
+            throw new Error('User not found');
+        }
+        user.lists.push(this._id);
+        await user.save();
+    }
+    next();
 });
 
 export const List = mongoose.model('List', listSchema);
--- src/database/models/UserSchema.js
+++ src/database/models/UserSchema.js
@@ -1,10 +1,21 @@
 import mongoose from 'mongoose';
-import { listSchema } from './ListSchema';
+import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    lists: [listSchema]
+    resetPasswordToken: { type: String },
+    resetPasswordExpires: { type: Date },
+    lists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
+});
+
+// Hash the password before saving the user
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
 });
 
 export const User = mongoose.model('User', userSchema);
