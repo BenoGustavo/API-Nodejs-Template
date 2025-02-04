533b4d3b624d07b2400cb2474324f86df7f32561
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-04 17:33:17 -0300

    Add UserRouter for user registration, login, password recovery, and user retrieval

---
+++ src/routes/user/UserRouter.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import { UserController } from '../../controllers/UserController';
+import { authMiddleware } from '../../middlewares/AuthMiddleware';
+import { UserService } from '../../services/UserService';
+
+const router = express.Router();
+const userService = new UserService();
+const userController = new UserController(userService);
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
+router.post('/recover-password', userController.recoverPassword);
+router.post('/send-recover-password-token/:email',userController.sendRecoverPasswordToken);
+router.get('/:id', authMiddleware, userController.getUserById);
+router.get('/username/:username', authMiddleware, userController.getUserByUsername);
+router.get('/email/:email', authMiddleware, userController.getUserByEmail);
+router.get('/', userController.getAllUsers);
+
+export {router as userRouter};
