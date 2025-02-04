88edad98067593758476477a9d51fd1dcc289601
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-04 17:33:23 -0300

    Refactor adaptMongooseError to handle BadRequest for duplicate key violations and preserve original error for image-related errors

--- src/errors/database/AdaptMongooseError.js
+++ src/errors/database/AdaptMongooseError.js
@@ -1,7 +1,8 @@
 import mongoose from 'mongoose';
-import { DuplicateKeyError, ValidationError, DatabaseError } from './DatabaseErrors';
+import { ValidationError, DatabaseError } from './DatabaseErrors';
 import { InvalidIdError } from '../InvalidIdError';
 import { NotFound } from '../http/NotFound';
+import { BadRequest } from '../http/BadRequest';
 
 /**
  * Factory function to handle database errors
@@ -9,10 +10,14 @@ import { NotFound } from '../http/NotFound';
  * @returns {Error}
  */
 export function adaptMongooseError(error) {
+    if (error.image != null) {
+        return error;
+    }
+
     if (error instanceof mongoose.Error.ValidationError) {
         return new ValidationError(error.message);
     } else if (error.code && error.code === 11000) {
-        return new DuplicateKeyError('Duplicate key error');
+        return new BadRequest('Unique key violation, duplicate key');
     } else if (error instanceof mongoose.Error.CastError) {
         return new InvalidIdError(`Invalid attribute ${error.path}: ${error.kind}`);
     } else if (error instanceof mongoose.Error.DocumentNotFoundError) {
