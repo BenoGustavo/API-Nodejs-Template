3905351e0a6df6e00bfd60678d560a15abee6f4c
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-06 17:05:48 -0300

    Enhance UserSchema by adding account activation fields and improving code formatting

--- src/database/models/UserSchema.js
+++ src/database/models/UserSchema.js
@@ -1,21 +1,24 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true, unique: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    resetPasswordToken: { type: String },
-    resetPasswordExpires: { type: Date },
-    lists: [{ type: mongoose.Schema.Types.ObjectId, ref: 'List' }]
+	username: { type: String, required: true, unique: true },
+	email: { type: String, required: true, unique: true },
+	password: { type: String, required: true },
+	isAccountActivated: { type: Boolean, default: false },
+	accountActivationToken: { type: String },
+	accountActivationTokenExpires: { type: Date },
+	resetPasswordToken: { type: String },
+	resetPasswordExpires: { type: Date },
+	lists: [{ type: mongoose.Schema.Types.ObjectId, ref: "List" }],
 });
 
 // Hash the password before saving the user
-userSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
-    const salt = await bcrypt.genSalt(10);
-    this.password = await bcrypt.hash(this.password, salt);
-    next();
+userSchema.pre("save", async function (next) {
+	if (!this.isModified("password")) return next();
+	const salt = await bcrypt.genSalt(10);
+	this.password = await bcrypt.hash(this.password, salt);
+	next();
 });
 
-export const User = mongoose.model('User', userSchema);
+export const User = mongoose.model("User", userSchema);
