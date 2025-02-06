812a2e38f4158353904d0c6aa70d96a9bc598c81
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-06 17:06:13 -0300

    Refactor UserRouter to improve code formatting and add account activation route

--- src/routes/user/UserRouter.js
+++ src/routes/user/UserRouter.js
@@ -1,19 +1,31 @@
-import express from 'express';
-import { UserController } from '../../controllers/UserController';
-import { authMiddleware } from '../../middlewares/AuthMiddleware';
-import { UserService } from '../../services/UserService';
+import express from "express";
+import { UserController } from "../../controllers/UserController";
+import { authMiddleware } from "../../middlewares/AuthMiddleware";
+import { UserService } from "../../services/UserService";
 
 const router = express.Router();
 const userService = new UserService();
 const userController = new UserController(userService);
 
-router.post('/register', userController.register);
-router.post('/login', userController.login);
-router.post('/recover-password', userController.recoverPassword);
-router.post('/send-recover-password-token/:email',userController.sendRecoverPasswordToken);
-router.get('/:id', authMiddleware, userController.getUserById);
-router.get('/username/:username', authMiddleware, userController.getUserByUsername);
-router.get('/email/:email', authMiddleware, userController.getUserByEmail);
-router.get('/', userController.getAllUsers);
+router.post("/register", userController.register);
+router.post("/login", userController.login);
+router.post("/recover-password", userController.recoverPassword);
 
-export {router as userRouter};
+router.post(
+	"/send-recover-password-token/:email",
+	userController.sendRecoverPasswordToken
+);
+
+router.get("/activate-account/:token", userController.activateAccount);
+router.get("/:id", authMiddleware, userController.getUserById);
+
+router.get(
+	"/username/:username",
+	authMiddleware,
+	userController.getUserByUsername
+);
+
+router.get("/email/:email", authMiddleware, userController.getUserByEmail);
+router.get("/", userController.getAllUsers);
+
+export { router as userRouter };
