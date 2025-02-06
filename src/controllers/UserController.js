8e7a7e77e8ed9b5196a380751ac42008895578d3
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-06 17:06:07 -0300

    Add account activation method to UserController and refactor code for consistency

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,185 +1,191 @@
-import { UserService } from '../services/UserService';
-import { ResponseDto } from '../dto/response/ResponseDto';
-import { Unauthorized } from '../errors/http/Unauthorized';
-import { NotFound } from '../errors/http/NotFound';
+import { UserService } from "../services/UserService";
+import { ResponseDto } from "../dto/response/ResponseDto";
+import { Unauthorized } from "../errors/http/Unauthorized";
+import { NotFound } from "../errors/http/NotFound";
 
 export class UserController {
+	/**
+	 * UserService constructor
+	 * @param {UserService} userService
+	 */
+	constructor(userService) {
+		this.userService = userService;
+		this.register = this.register.bind(this);
+		this.login = this.login.bind(this);
+		this.recoverPassword = this.recoverPassword.bind(this);
+		this.getUserById = this.getUserById.bind(this);
+		this.getUserByUsername = this.getUserByUsername.bind(this);
+		this.getUserByEmail = this.getUserByEmail.bind(this);
+		this.sendRecoverPasswordToken = this.sendRecoverPasswordToken.bind(this);
+		this.getAllUsers = this.getAllUsers.bind(this);
+		this.activateAccount = this.activateAccount.bind(this);
+	}
 
-    /**
-     * UserService constructor
-     * @param {UserService} userService
-    */
-    constructor(userService) {
-        this.userService = userService;
-        this.register = this.register.bind(this);
-        this.login = this.login.bind(this);
-        this.recoverPassword = this.recoverPassword.bind(this);
-        this.getUserById = this.getUserById.bind(this);
-        this.getUserByUsername = this.getUserByUsername.bind(this);
-        this.getUserByEmail = this.getUserByEmail.bind(this);
-        this.sendRecoverPasswordToken = this.sendRecoverPasswordToken.bind(this);
-        this.getAllUsers = this.getAllUsers.bind(this);
-    }
+	async register(req, res, next) {
+		try {
+			const { user, token } = await this.userService.register(
+				req.body,
+				req.get("origin")
+			);
 
-    async register(req, res, next) {
-        try {
-            const { user, token } = await this.userService.register(req.body);
+			const response = new ResponseDto(201, "User registered successfully", {
+				token,
+				user,
+			});
 
-            const response = new ResponseDto(
-                201,
-                'User registered successfully',
-                {token,user}
-            );
+			res.status(201).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-            res.status(201).json(response);
-        } catch (error) {
-            next(error);
-        }
-    }
+	/**
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 * @param {import('express').NextFunction} next
+	 */
+	async activateAccount(req, res, next) {
+		try {
+			const { token } = req.params;
+			const user = await this.userService.activateAccount(token);
+			const response = new ResponseDto(
+				200,
+				"User activated successfully",
+				user
+			);
+			res.status(200).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    async login(req, res, next) {
-        try {
-            const { user, token } = await this.userService.login(req.body);
+	async login(req, res, next) {
+		try {
+			const { user, token } = await this.userService.login(req.body);
 
-            const response = new ResponseDto(
-                200,
-                'User logged in successfully',
-                {token,user}
-            );
+			const response = new ResponseDto(200, "User logged in successfully", {
+				token,
+				user,
+			});
 
-            res.status(200).json(response);
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(200).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    async recoverPassword(req, res, next) {
-        try {
-            const token = await this.userService.recoverPassword(req.body);
+	async recoverPassword(req, res, next) {
+		try {
+			const token = await this.userService.recoverPassword(req.body);
 
-            const response = new ResponseDto(
-                200,
-                'Password recovery email sent',
-                {token}
-            );
+			const response = new ResponseDto(200, "Password recovery email sent", {
+				token,
+			});
 
-            res.status(200).json(response);
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(200).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    async sendRecoverPasswordToken(req, res, next) {
-        try {
-            console.log(
-                this.constructor.name
-            )
+	async sendRecoverPasswordToken(req, res, next) {
+		try {
+			console.log(this.constructor.name);
 
-            const token = await this.userService.sendRecoverPasswordToken(req.params.email);
+			const token = await this.userService.sendRecoverPasswordToken(
+				req.params.email
+			);
 
-            const response = new ResponseDto(
-                200,
-                'Password recovery email sent',
-                token
-            );
+			const response = new ResponseDto(
+				200,
+				"Password recovery email sent",
+				token
+			);
 
-            res.status(200).json(response);
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(200).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    async getUserById(req, res, next) {
-        try {
-            const user = await this.userService.getUserById(req.params.id);
+	async getUserById(req, res, next) {
+		try {
+			const user = await this.userService.getUserById(req.params.id);
 
-            if (!user) {
-                throw new NotFound('User not found');
-            }
+			if (!user) {
+				throw new NotFound("User not found");
+			}
 
-            if (user.id !== req.user.id) {
-                throw new Unauthorized('You can only access your own user information');
-            }
+			if (user.id !== req.user.id) {
+				throw new Unauthorized("You can only access your own user information");
+			}
 
-            const response = new ResponseDto(
-                200,
-                'User found',
-                user
-            );
+			const response = new ResponseDto(200, "User found", user);
 
-            res.status(200).json(response);
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(200).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    async getAllUsers(req, res, next) {
-        try {
-            const users = await this.userService.getAllUsers(
-                req.query.page,
-                req.query.limit,
-                req.query.sort,
-                req.query.order,
-                req.query.search
-            );
+	async getAllUsers(req, res, next) {
+		try {
+			const users = await this.userService.getAllUsers(
+				req.query.page,
+				req.query.limit,
+				req.query.sort,
+				req.query.order,
+				req.query.search
+			);
 
-            const response = new ResponseDto(
-                200,
-                'Users found',
-                users
-            );
+			const response = new ResponseDto(200, "Users found", users);
 
-            res.status(200).json(response);
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(200).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    async getUserByUsername(req, res, next) {
-        try {
-            const user = await this.userService.getUserByUsername(req.params.username);
+	async getUserByUsername(req, res, next) {
+		try {
+			const user = await this.userService.getUserByUsername(
+				req.params.username
+			);
 
-            if (!user) {
-                throw new NotFound('User not found');
-            }
+			if (!user) {
+				throw new NotFound("User not found");
+			}
 
-            if (user.id !== req.user.id) {
-                throw new Unauthorized('You can only access your own user information');
-            }
+			if (user.id !== req.user.id) {
+				throw new Unauthorized("You can only access your own user information");
+			}
 
-            const response = new ResponseDto(
-                200,
-                'User found',
-                user
-            );
+			const response = new ResponseDto(200, "User found", user);
 
-            res.status(200).json(response);
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(200).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    async getUserByEmail(req, res, next) {
-        try {
-            const user = await this.userService.getUserByEmail(req.params.email);
+	async getUserByEmail(req, res, next) {
+		try {
+			const user = await this.userService.getUserByEmail(req.params.email);
 
-            if (!user) {
-                throw new NotFound('User not found');
-            }
+			if (!user) {
+				throw new NotFound("User not found");
+			}
 
-            if (user.id !== req.user.id) {
-                throw new Unauthorized('You can only access your own user information');
-            }
+			if (user.id !== req.user.id) {
+				throw new Unauthorized("You can only access your own user information");
+			}
 
-            const response = new ResponseDto(
-                200,
-                'User found',
-                user
-            );
+			const response = new ResponseDto(200, "User found", user);
 
-            res.status(200).json(response);
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(200).json(response);
+		} catch (error) {
+			next(error);
+		}
+	}
 }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,160 +1,239 @@
-import { User } from '../database/models/UserSchema';
-import bcrypt from 'bcryptjs';
-import crypto from 'crypto';
-import { adaptMongooseError } from '../errors/database/AdaptMongooseError';
-import { JwtService } from './JwtService';
-import { UserRegisterDto } from '../dto/user/UserRegisterDto';
-import { UserLoginDto } from '../dto/user/UserLoginDto';
-import { UserResetPasswordDto } from '../dto/user/UserResetPasswordDto';
-import { BadRequest } from '../errors/http/BadRequest';
-import { NotFound } from '../errors/http/NotFound';
+import { User } from "../database/models/UserSchema";
+import bcrypt from "bcryptjs";
+import crypto from "crypto";
+import { adaptMongooseError } from "../errors/database/AdaptMongooseError";
+import { JwtService } from "./JwtService";
+import { UserRegisterDto } from "../dto/user/UserRegisterDto";
+import { UserLoginDto } from "../dto/user/UserLoginDto";
+import { UserResetPasswordDto } from "../dto/user/UserResetPasswordDto";
+import { BadRequest } from "../errors/http/BadRequest";
+import { NotFound } from "../errors/http/NotFound";
+import { emailSenderInstance } from "../components/EmailSender";
 
 export class UserService {
-    /**
-     * Register a new user
-     * @param {UserRegisterDto} data 
-    **/
-    async register(data) {
-        try {
-            if (data.password !== data.confirmPassword) {
-                throw new BadRequest('Passwords do not match');
-            }
+	/**
+	 * Register a new user
+	 * @param {UserRegisterDto} data
+	 * @param {string} baseUrl
+	 **/
+	async register(data, baseUrl) {
+		try {
+			if (data.password !== data.confirmPassword) {
+				throw new BadRequest("Passwords do not match");
+			}
 
-            const user = new User();
-            user.username = data.username;
-            user.email = data.email;
-            user.password = data.password;
-            user.lists = [];
-            
+			const token = crypto.randomBytes(35).toString("hex");
 
-            await user.save();
-            const token = JwtService.generateToken(user);
-            return { user, token };
-        } catch (error) {
-            throw adaptMongooseError(error);
-        }
-    }
+			const user = new User();
+			user.username = data.username;
+			user.email = data.email;
+			user.password = data.password;
+			user.lists = [];
+			user.accountActivationToken = token;
+			user.accountActivationTokenExpires = Date.now() + 3600000; // 1 hour
 
-    /**
-     * Try to login a user
-     * @param {UserLoginDto} data 
-    **/
-    async login(data) {
-        const { email, password } = data;
+			// Validates if the user already exists and the account is activated
+			const existingUser = await User.findOne({ email: data.email });
+			if (existingUser) {
+				if (existingUser.isAccountActivated) {
+					throw new BadRequest("Email already exists");
+				}
+				await existingUser.deleteOne();
+			}
 
-        const user = await User.findOne({ email });
-        if (!user) {
-            throw new BadRequest('User not found');
-        }
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            throw new BadRequest('Invalid credentials');
-        }
-        const token = JwtService.generateToken(user);
-        return { user, token };
-    }
+			await user.save();
+			const jwtToken = JwtService.generateToken(user);
 
-    /**
-     * Send the recover password token to the user by email
-     * @param {string} email 
-    **/
-    async sendRecoverPasswordToken(email) {
-        const user = await User.findOne({ email });
-        if (!user) {
-            throw new NotFound('User not found');
-        }
-        const token = crypto.randomBytes(20).toString('hex');
-        user.resetPasswordToken = token;
-        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+			// Should send an email with the token
+			//TODO: SHOULD SEND THE USER TO THE FRONT-END TO ACTIVATE THE ACCOUNT
+			const accountActivationUrl = `${baseUrl}/api/user/activate-account/${token}`;
 
-        // Should send an email with the token
-        console.log(
-            `\n🔑 Password recovery token for ${user.email}: ${token} 🔑'`
-        )
+			const email = emailSenderInstance.createNewEmail(
+				user.email,
+				"Account activation",
+				`Hello ${user.username},\n\n` +
+					`Please click on the following link to activate your account:\n\n` +
+					`${accountActivationUrl}\n\n` +
+					"If you did not request this, please ignore this email and your password will remain unchanged.\n",
+				`Hello <b>${user.username}</b>,<br><br>` +
+					`Please click on the following link to activate your account:<br><br>` +
+					`<a href="${accountActivationUrl}">Activate account</a><br><br>` +
+					"If you did not request this, please ignore this email and your password will remain unchanged.<br>"
+			);
 
-        await user.save();
-        return token;
-    }
+			emailSenderInstance.sendEmail(email);
 
-    /**
-     * Resets the user password
-     * @param {UserResetPasswordDto} data 
-    **/
-    async recoverPassword(data) {
-        const { token, password, confirmPassword } = data;
+			return { user, jwtToken };
+		} catch (error) {
+			throw adaptMongooseError(error);
+		}
+	}
 
-        if (password !== confirmPassword) {
-            throw new BadRequest('Passwords do not match');
-        }
+	/**
+	 *
+	 * @param {string} token
+	 * @returns
+	 */
+	async activateAccount(token) {
+		const user = await User.findOne({
+			accountActivationToken: token,
+			accountActivationTokenExpires: { $gt: Date.now() },
+		});
 
-        const user = await User.findOne({
-            resetPasswordToken: token,
-            resetPasswordExpires: { $gt: Date.now() }
-        });
+		if (!user) {
+			throw new NotFound("Account activation token is invalid or has expired");
+		}
 
-        if (!user) {
-            throw new BadRequest('Password reset token is invalid or has expired');
-        }
+		user.isAccountActivated = true;
 
-        user.password = password;
-        user.resetPasswordToken = undefined;
-        user.resetPasswordExpires = undefined;
+		try {
+			await user.save();
+		} catch (error) {
+			throw adaptMongooseError(error);
+		}
 
-        try {
-            await user.save();
-        } catch (error) {
-            throw adaptMongooseError(error);
-        }
+		return user;
+	}
 
-        return user;
-    }
+	/**
+	 * Try to login a user
+	 * @param {UserLoginDto} data
+	 **/
+	async login(data) {
+		const { email, password } = data;
 
-    /**
-     * get a user by id
-     * @param {string} id 
-    **/
-    async getUserById(id) {
-        return await User.findById(id);
-    }
+		const user = await User.findOne({ email });
 
-    /**
-     * get a user by username
-     * @param {string} username 
-    **/
-    async getUserByUsername(username) {
-        return await User.findOne({ username });
-    }
+		if (!user) {
+			throw new BadRequest("User not found");
+		}
 
-    /**
-     * get a user by email
-     * @param {string} email 
-    **/
-    async getUserByEmail(email) {
-        return await User.findOne({ email });
-    }
+		// Check if the account is not activated and the token has expired, then delete the user
+		if (
+			!user.isAccountActivated &&
+			user.accountActivationTokenExpires < Date.now()
+		) {
+			await user.deleteOne();
+			throw new BadRequest("User not found");
+		}
 
-    /**
-     * get all users
-     * @param {number} page
-     * @param {number} limit
-     * @param {string} sort
-     * @param {string} order
-     * @param {string} search
-    **/
-    async getAllUsers(page = 1, limit = 10, sort = 'createdAt', order = 'desc', search = '') {
-        const query = {};
-        if (search) {
-            query.$or = [
-                { username: { $regex: search, $options: 'i' } },
-                { email: { $regex: search, $options: 'i' } }
-            ];
-        }
+		if (!user.isAccountActivated) {
+			throw new BadRequest("Account is not activated, please check your email");
+		}
 
-        const users = await User.find(query)
-            .sort({ [sort]: order })
-            .skip((page - 1) * limit)
-            .limit(limit);
+		const isMatch = await bcrypt.compare(password, user.password);
+		if (!isMatch) {
+			throw new BadRequest("Invalid credentials");
+		}
+		const token = JwtService.generateToken(user);
+		return { user, token };
+	}
 
-        return users;
-    }
+	/**
+	 * Send the recover password token to the user by email
+	 * @param {string} email
+	 **/
+	async sendRecoverPasswordToken(email) {
+		const user = await User.findOne({ email });
+		if (!user) {
+			throw new NotFound("User not found");
+		}
+		const token = crypto.randomBytes(15).toString("hex");
+		user.resetPasswordToken = token;
+		user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+		// Should send an email with the token
+		console.log(`\n🔑 Password recovery token for ${user.email}: ${token} 🔑'`);
+
+		await user.save();
+		return token;
+	}
+
+	/**
+	 * Resets the user password
+	 * @param {UserResetPasswordDto} data
+	 **/
+	async recoverPassword(data) {
+		const { token, password, confirmPassword } = data;
+
+		if (password !== confirmPassword) {
+			throw new BadRequest("Passwords do not match");
+		}
+
+		const user = await User.findOne({
+			resetPasswordToken: token,
+			resetPasswordExpires: { $gt: Date.now() },
+		});
+
+		if (!user) {
+			throw new BadRequest("Password reset token is invalid or has expired");
+		}
+
+		user.password = password;
+		user.resetPasswordToken = undefined;
+		user.resetPasswordExpires = undefined;
+
+		try {
+			await user.save();
+		} catch (error) {
+			throw adaptMongooseError(error);
+		}
+
+		return user;
+	}
+
+	/**
+	 * get a user by id
+	 * @param {string} id
+	 **/
+	async getUserById(id) {
+		return await User.findById(id);
+	}
+
+	/**
+	 * get a user by username
+	 * @param {string} username
+	 **/
+	async getUserByUsername(username) {
+		return await User.findOne({ username });
+	}
+
+	/**
+	 * get a user by email
+	 * @param {string} email
+	 **/
+	async getUserByEmail(email) {
+		return await User.findOne({ email });
+	}
+
+	/**
+	 * get all users
+	 * @param {number} page
+	 * @param {number} limit
+	 * @param {string} sort
+	 * @param {string} order
+	 * @param {string} search
+	 **/
+	async getAllUsers(
+		page = 1,
+		limit = 10,
+		sort = "createdAt",
+		order = "desc",
+		search = ""
+	) {
+		const query = {};
+		if (search) {
+			query.$or = [
+				{ username: { $regex: search, $options: "i" } },
+				{ email: { $regex: search, $options: "i" } },
+			];
+		}
+
+		const users = await User.find(query)
+			.sort({ [sort]: order })
+			.skip((page - 1) * limit)
+			.limit(limit);
+
+		return users;
+	}
 }
