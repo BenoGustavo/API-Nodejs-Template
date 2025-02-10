f15c626805423e6a0c516bc99fdab6e8b5a96ba5
Gustavo <gustavoleandrogorges@gmail.com>
2025-02-10 11:16:20 -0300

    Refactor ToDoController to improve code readability and maintainability

--- src/controllers/ToDoController.js
+++ src/controllers/ToDoController.js
@@ -1,124 +1,142 @@
 import { ToDoService } from "../services/ToDoService";
 
 export class ToDoController {
+	/**
+	 * ListController constructor
+	 * @param {ToDoService} toDoService
+	 */
+	constructor(toDoService) {
+		this.toDoService = toDoService;
 
-    /**
-     * ListController constructor
-     * @param {ToDoService} toDoService
-    */
-    constructor(toDoService) {
-        this.toDoService = toDoService;
+		this.createToDo = this.createToDo.bind(this);
+		this.getToDos = this.getToDos.bind(this);
+		this.getToDosByListId = this.getToDosByListId.bind(this);
+		this.getToDoById = this.getToDoById.bind(this);
+		this.updateToDo = this.updateToDo.bind(this);
+		this.deleteToDo = this.deleteToDo.bind(this);
+	}
 
-        this.createToDo = this.createToDo.bind(this);
-        this.getToDos = this.getToDos.bind(this);
-        this.getToDosByListId = this.getToDosByListId.bind(this);
-        this.getToDoById = this.getToDoById.bind(this);
-        this.updateToDo = this.updateToDo.bind(this);
-        this.deleteToDo = this.deleteToDo.bind(this);
-    }
+	/**
+	 *
+	 *  Creates a new ToDo
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 *
+	 * @returns {Promise<Response>}
+	 */
+	async createToDo(req, res, next) {
+		try {
+			const toDo = await this.toDoService.createToDo(
+				req.params.listId,
+				req.user.id,
+				req.body
+			);
+			res.status(201).json(toDo);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    /**
-     * 
-     *  Creates a new ToDo
-     * 
-     * @param {Request} req 
-     * @param {Response} res 
-     * 
-     * @returns {Promise<Response>}
-    */
-    async createToDo(req, res, next) {
-        try {
-            const toDo = await this.toDoService.createToDo(req.params.listId,req.user.id,req.body);
-            res.status(201).json(toDo);
-        } catch (error) {
-            next(error);
-        }
-    }
+	/**
+	 *
+	 *  Retrieves all ToDos from database
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 *
+	 * @returns {Promise<Response>}
+	 */
+	async getToDos(req, res, next) {
+		try {
+			const userId = req.user.id;
 
-    /**
-     * 
-     *  Retrieves all ToDos from database
-     * 
-     * @param {Request} req 
-     * @param {Response} res 
-     * 
-     * @returns {Promise<Response>}
-    */
-   async getToDos(req, res, next) {
-       try {
-           const toDos = await this.toDoService.getToDos();
-           res.status(200).json(toDos);
-       } catch (error) {
-           next(error);
-       }
-   }
+			const toDos = await this.toDoService.getToDos(userId);
+			res.status(200).json(toDos);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-   async getToDosByListId(req, res, next) {
-         try {
-              const toDos = await this.toDoService.getToDosByListId(req.user.id,req.params.id);
+	async getToDosByListId(req, res, next) {
+		try {
+			const toDos = await this.toDoService.getToDosByListId(
+				req.user.id,
+				req.params.id
+			);
 
-              res.status(200).json(toDos);
-         } catch (error) {
-              next(error);
-         }
-   }
+			res.status(200).json(toDos);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    /**
-     * 
-     * Get a ToDo by id
-     * 
-     * @param {Request} req 
-     * @param {Response} res 
-     * 
-     * @returns {Promise<Response>}
-    */
-    async getToDoById(req, res, next) {
-        try {
-            const toDo = await this.toDoService.getToDoById(req.user.id,req.params.id);
-            if (!toDo) {
-                return res.status(404).json({ error: 'ToDo not found' });
-            }
-            res.status(200).json(toDo);
-        } catch (error) {
-            next(error);
-        }
-    }
+	/**
+	 *
+	 * Get a ToDo by id
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 *
+	 * @returns {Promise<Response>}
+	 */
+	async getToDoById(req, res, next) {
+		try {
+			const toDo = await this.toDoService.getToDoById(
+				req.user.id,
+				req.params.id
+			);
+			if (!toDo) {
+				return res.status(404).json({ error: "ToDo not found" });
+			}
+			res.status(200).json(toDo);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    /**
-     * 
-     * Updates a ToDo
-     * 
-     * @param {Request} req 
-     * @param {Response} res 
-     * 
-     * @returns {Promise<Response>}
-    */
-    async updateToDo(req, res, next) {
-        try {
-            const toDo = await this.toDoService.updateToDo(req.params.id,req.user.id,req.body);
+	/**
+	 *
+	 * Updates a ToDo
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 *
+	 * @returns {Promise<Response>}
+	 */
+	async updateToDo(req, res, next) {
+		try {
+			const toDo = await this.toDoService.updateToDo(
+				req.params.id,
+				req.user.id,
+				req.body
+			);
 
-            res.status(200).json(toDo);
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(200).json(toDo);
+		} catch (error) {
+			next(error);
+		}
+	}
 
-    /**
-     * 
-     * Deletes a ToDo
-     * 
-     * @param {Request} req 
-     * @param {Response} res 
-     * 
-     * @returns {Promise<Response>}
-    */
-    async deleteToDo(req, res, next) {
-        try {
-            const toDo = await this.toDoService.deleteToDo(req.user.id,req.params.id);
+	/**
+	 *
+	 * Deletes a ToDo
+	 *
+	 * @param {Request} req
+	 * @param {Response} res
+	 *
+	 * @returns {Promise<Response>}
+	 */
+	async deleteToDo(req, res, next) {
+		try {
+			const toDo = await this.toDoService.deleteToDo(
+				req.user.id,
+				req.params.id
+			);
 
-            res.status(204).send();
-        } catch (error) {
-            next(error);
-        }
-    }
+			res.status(204).send();
+		} catch (error) {
+			next(error);
+		}
+	}
 }
--- src/services/ToDoService.js
+++ src/services/ToDoService.js
@@ -1,177 +1,196 @@
-import { ToDo } from '../database/models/TodoSchema';
-import { List } from '../database/models/ListSchema';
-import { TodoDto } from '../dto/TodoDto';
-import mongoose from 'mongoose';
-import { InvalidIdError } from '../errors/InvalidIdError';
-import { NotFound } from '../errors/http/NotFound';
-import { Unauthorized } from '../errors/http/Unauthorized';
-// import { BadRequest } from '../errors/http/BadRequest';
-import { adaptMongooseError } from '../errors/database/AdaptMongooseError';
+import { ToDo } from "../database/models/TodoSchema";
+import { List } from "../database/models/ListSchema";
+import { TodoDto } from "../dto/TodoDto";
+import mongoose from "mongoose";
+import { User } from "../database/models/UserSchema";
+import { NotFound } from "../errors/http/NotFound";
+import { Unauthorized } from "../errors/http/Unauthorized";
+import { adaptMongooseError } from "../errors/database/AdaptMongooseError";
+import { Forbidden } from "../errors/http/Forbidden";
 
 export class ToDoService {
-    constructor() {
-        this.createToDo = this.createToDo.bind(this);
-        this.getToDos = this.getToDos.bind(this);
-        this.getToDoById = this.getToDoById.bind(this);
-        this.updateToDo = this.updateToDo.bind(this);
-        this.deleteToDo = this.deleteToDo.bind(this);
-    }
+	constructor() {
+		this.createToDo = this.createToDo.bind(this);
+		this.getToDos = this.getToDos.bind(this);
+		this.getToDoById = this.getToDoById.bind(this);
+		this.updateToDo = this.updateToDo.bind(this);
+		this.deleteToDo = this.deleteToDo.bind(this);
+	}
 
-    /**
-     * Creates a new ToDo
-     * @param {string} listId
-     * @param {string} userRequesterId
-     * @param {TodoDto} data
-     * @returns {ToDo}
-     */
-    async createToDo(listId,userRequesterId,data) {
-        try {
-            const list = await List.findById(listId);
+	/**
+	 * Creates a new ToDo
+	 * @param {string} listId
+	 * @param {string} userRequesterId
+	 * @param {TodoDto} data
+	 * @returns {ToDo}
+	 */
+	async createToDo(listId, userRequesterId, data) {
+		try {
+			const list = await List.findById(listId);
 
-            if(list.user != userRequesterId){
-                throw new Unauthorized('You are not allowed to create a ToDo in this list, try creating a new list');
-            }
+			if (list.user != userRequesterId) {
+				throw new Unauthorized(
+					"You are not allowed to create a ToDo in this list, try creating a new list"
+				);
+			}
 
-            if (!list) {
-                throw new NotFound('List not found');
-            }
+			if (!list) {
+				throw new NotFound("List not found");
+			}
 
-            const toDo = new ToDo(data);
+			const toDo = new ToDo(data);
 
-            list.items.push(toDo);
+			list.items.push(toDo);
 
-            await list.save();
-            return await toDo.save();
-        } catch (error) {
-            throw adaptMongooseError(error);
-        }
-    }
+			await list.save();
+			return await toDo.save();
+		} catch (error) {
+			throw adaptMongooseError(error);
+		}
+	}
 
-    /**
-     * Gets all ToDos
-     * @returns {ToDo[]}
-     */
-    async getToDos() {
-        try {
-            return await ToDo.find();
-        } catch (error) {
-            throw adaptMongooseError(error);
-        }
-    }
+	/**
+	 * Gets all ToDos
+	 * @param {String} userRequesterId
+	 *
+	 * @returns {ToDo[]}
+	 */
+	async getToDos(userRequesterId) {
+		const user = await User.findById(userRequesterId);
 
+		if (!(user.role === "admin")) {
+			throw new Forbidden(
+				"You're not authorized to access this route, because you don't have the necessary permissions"
+			);
+		}
 
-    /**
-     * Gets all ToDos from a list
-     * 
-     * @param {string} userRequesterId
-     * @param {string} id
-     * @returns {List<ToDo[]>}
-     */
-    async getToDosByListId(userRequesterId,id) {
-        try {
-            const list = await List.findById(id).populate('items');
-            
-            if(!list){
-                throw new NotFound("To-do List not found, parhaps it doesn't exists or the id might be invalid")
-            }
+		return await ToDo.find();
+	}
 
-            if (list.user != userRequesterId) {
-                throw new Unauthorized('You are not allowed to view this list because its not yours, try creating a new list');
-            }
+	/**
+	 * Gets all ToDos from a list
+	 *
+	 * @param {string} userRequesterId
+	 * @param {string} id
+	 * @returns {List<ToDo[]>}
+	 */
+	async getToDosByListId(userRequesterId, id) {
+		try {
+			const list = await List.findById(id).populate("items");
 
-            return list
-        } catch (error) {
-            throw adaptMongooseError(error);
-        }
-    }
+			if (!list) {
+				throw new NotFound(
+					"To-do List not found, parhaps it doesn't exists or the id might be invalid"
+				);
+			}
 
-    /**
-     * Gets a ToDo by id
-     * 
-     * @param {string} userRequesterId
-     * @param {string} id
-     * @returns {ToDo}
-     */
-    async getToDoById(userRequesterId,id) {
-        if (!mongoose.Types.ObjectId.isValid(id)) {
-            throw new NotFound("To-do not found, parhaps it doesn't exists or the id might be invalid")
-        }
+			if (list.user != userRequesterId) {
+				throw new Unauthorized(
+					"You are not allowed to view this list because its not yours, try creating a new list"
+				);
+			}
 
-        const toDo = await ToDo.findById(id);
-        const list = await List.findOne({ items: id });
+			return list;
+		} catch (error) {
+			throw adaptMongooseError(error);
+		}
+	}
 
-        if (!toDo) {
-            throw new NotFound('ToDo not found');
-        }
+	/**
+	 * Gets a ToDo by id
+	 *
+	 * @param {string} userRequesterId
+	 * @param {string} id
+	 * @returns {ToDo}
+	 */
+	async getToDoById(userRequesterId, id) {
+		if (!mongoose.Types.ObjectId.isValid(id)) {
+			throw new NotFound(
+				"To-do not found, parhaps it doesn't exists or the id might be invalid"
+			);
+		}
 
-        if (list.user != userRequesterId) {
-            throw new Unauthorized('You are not allowed to view this ToDo');
-        }
+		const toDo = await ToDo.findById(id);
+		const list = await List.findOne({ items: id });
 
-        return toDo;
-    }
+		if (!toDo) {
+			throw new NotFound("ToDo not found");
+		}
 
-    /**
-     * Updates a ToDo
-     * @param {string} id
-     * @param {string} userRequesterId
-     * @param {TodoDto} data
-     * @returns {ToDo}
-     */
-    async updateToDo(id,userRequesterId,data) {
-        try {
-            const toDoList = await List.findOne({ items: id });
-            const toDoOwner = toDoList.user ?? null;
+		if (list.user != userRequesterId) {
+			throw new Unauthorized("You are not allowed to view this ToDo");
+		}
 
-            if (!toDoOwner) {
-                throw new NotFound('ToDo not found');
-            }
+		return toDo;
+	}
 
-            if (toDoOwner._id.toString() !== userRequesterId) {
-                throw new Unauthorized('You are not allowed to update this ToDo');
-            }
+	/**
+	 * Updates a ToDo
+	 * @param {string} id
+	 * @param {string} userRequesterId
+	 * @param {TodoDto} data
+	 * @returns {ToDo}
+	 */
+	async updateToDo(id, userRequesterId, data) {
+		try {
+			const toDoList = await List.findOne({ items: id });
+			const toDoOwner = toDoList.user ?? null;
 
-            const toDo = await ToDo.findByIdAndUpdate(id, data, { new: true, runValidators: true });
+			if (!toDoOwner) {
+				throw new NotFound("ToDo not found");
+			}
 
-            if(!toDo){
-                throw new NotFound("To-do not found, parhaps it doesn't exists or the id might be invalid")
-            }
+			if (toDoOwner._id.toString() !== userRequesterId) {
+				throw new Unauthorized("You are not allowed to update this ToDo");
+			}
 
-            return toDo;
-        } catch (error) {
-            throw adaptMongooseError(error);
-        }
-    }
+			const toDo = await ToDo.findByIdAndUpdate(id, data, {
+				new: true,
+				runValidators: true,
+			});
 
-    /**
-     * Deletes a ToDo
-     * @param {string} userRequesterId
-     * @param {string} id
-     * @returns {void}
-     */
-    async deleteToDo(userRequesterId,id) {
-        try {
+			if (!toDo) {
+				throw new NotFound(
+					"To-do not found, parhaps it doesn't exists or the id might be invalid"
+				);
+			}
 
-            const toDoList = await List.findOne({ items: id })
-            const toDoOwner = toDoList.user ?? null;
+			return toDo;
+		} catch (error) {
+			throw adaptMongooseError(error);
+		}
+	}
 
-            if (!toDoOwner) {
-                throw new NotFound('ToDo not found');
-            }
+	/**
+	 * Deletes a ToDo
+	 * @param {string} userRequesterId
+	 * @param {string} id
+	 * @returns {void}
+	 */
+	async deleteToDo(userRequesterId, id) {
+		try {
+			const toDoList = await List.findOne({ items: id });
+			const toDoOwner = toDoList.user ?? null;
 
-            if (toDoOwner._id.toString() !== userRequesterId) {
-                throw new Unauthorized('You are not allowed to update this ToDo');
-            }
+			if (!toDoOwner) {
+				throw new NotFound("ToDo not found");
+			}
 
-            const toDo = await ToDo.findOneAndDelete(id);
+			if (toDoOwner._id.toString() !== userRequesterId) {
+				throw new Unauthorized("You are not allowed to update this ToDo");
+			}
 
-            if(!toDo){
-                throw new NotFound("To-do not found, parhaps it doesn't exists or the id might be invalid")
-            }
+			const toDo = await ToDo.findOneAndDelete(id);
 
-            return toDo;
-        } catch (error) {
-            throw adaptMongooseError(error);
-        }
-    }
+			if (!toDo) {
+				throw new NotFound(
+					"To-do not found, parhaps it doesn't exists or the id might be invalid"
+				);
+			}
+
+			return toDo;
+		} catch (error) {
+			throw adaptMongooseError(error);
+		}
+	}
 }
